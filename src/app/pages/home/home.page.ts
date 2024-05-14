9a8895ccfb83d9f9051842e8cfd800e8dda263d1
Rodrigo Oliveira <rodrigofeitosa.contato@gmail.com>
2024-05-14 18:04:04 -0300

    feat: implement automatic greeting based on time of day

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  public greeting!: String
+  constructor() { }
 
-  constructor() {}
+  generateGreeting() {
+    const timeOfDay = new Date().getHours()
+
+    if (timeOfDay < 12) {
+      return 'Bom dia'
+    } else if (timeOfDay < 18) {
+      return 'Boa tarde'
+    } else {
+      return 'Boa noite'
+    }
+  }
+
+  ngOnInit(): void {
+    this.greeting = this.generateGreeting()
+  }
 
 }
