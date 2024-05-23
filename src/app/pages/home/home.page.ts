ab107faa6b6d4fc4c3800b808adf8b91ff82c7e9
Rodrigo Oliveira <rodrigofeitosa.contato@gmail.com>
2024-05-22 21:35:28 -0300

    feat: integrate auth service in home page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import axios from 'axios';
-import { CookieService } from 'ngx-cookie-service';
+
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -9,11 +9,11 @@ import { CookieService } from 'ngx-cookie-service';
 })
 export class HomePage implements OnInit {
   public greeting!: String
-  public userName!: String
+  public userNameFormatted: string = ""
+  public days_in_offensive: number = 0
 
-  private apiUrl: string = 'https://api-get-a-diet-production.up.railway.app'
 
-  constructor(private cookies: CookieService) { }
+  constructor(private AuthService: AuthService) { }
 
   generateGreeting() {
     const timeOfDay = new Date().getHours()
@@ -29,37 +29,25 @@ export class HomePage implements OnInit {
 
   async fetchData() {
     const loginInfo = {
-      "email": "johndoe@example4.com",
+      "email": "johndoe@example5.com",
       "password": "123456"
     }
 
-    const authResponse = await axios.post(`${this.apiUrl}/sessions`, loginInfo)
+    await this.AuthService.authenticate(loginInfo)
 
-    const { token } = authResponse.data
-
-    this.cookies.set('access-token', token)
-
-    const userDataResponse = await axios.get(`${this.apiUrl}/me`, {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    })
-
-    return userDataResponse.data.user
-
-
+    await this.AuthService.fetchUser()
   }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     this.greeting = this.generateGreeting()
 
-    this.fetchData().then(userDataResponse => {
-      const userNameFormatted = userDataResponse.name.split(" ")[0]
-      this.userName = userNameFormatted
+    if (!this.AuthService.user) {
+      await this.fetchData()
     }
-    )
 
-
+    if (this.AuthService.user) {
+      this.userNameFormatted = this.AuthService.user.name.split(" ")[0];
+      this.days_in_offensive = this.AuthService.user.days_in_offensive;
+    }
   }
-
 }
